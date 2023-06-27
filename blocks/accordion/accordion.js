2717e9c27abf389116b3fb4b5ec76b557d43e4f1
Jordan Sheen <sheen@adobe.com>
2023-06-27 16:50:19 -0400

    Add comments to the accordion block.

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -2,23 +2,36 @@ import { } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
     /* Get all of the rows, which are their own accordion button and panels */
-    /* For each panel, set the display to 'none' */
     [...block.children].forEach((row) => {
+         /* For each panel, set the display to 'none' */
          row.children[1].style.display = 'none';
+
+         /* For the first child (the button name column) add the button and class */
          const firstChild = row.firstElementChild;
          firstChild.classList.add('button');
+
+         /* Add the button listener */
          firstChild.addEventListener('click', () => {
+
+            /* The row's second child is the panel. Add the panel class and display as block */
             const panel = row.children[1];
-                panel.classList.add('panel');
-                const isPanelVisible = panel.style.display === 'block';
-                if (panel.style.display === 'none') {
-                    panel.style.display = 'block';
-                } else {
-                    panel.style.display = 'none';
-                }
-                if (!isPanelVisible) {
-                    panel.scrollIntoView({ behavior: 'smooth'});
-                }
+            panel.classList.add('panel');
+            const isPanelVisible = panel.style.display === 'block';
+
+            /* If the panel is not displayed on click */
+            if (panel.style.display === 'none') {
+
+                /* Display it */
+                panel.style.display = 'block';
+            } else {
+                /* Hide it */
+                panel.style.display = 'none';
+            }
+
+            /* Add smooth scrolling */
+            if (!isPanelVisible) {
+                panel.scrollIntoView({ behavior: 'smooth'});
+            }
          })
     })
 };
